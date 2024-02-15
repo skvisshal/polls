f4ff70a9f1d17f692dfdb6ad91797fe26d03b515
Visshal Suresh <skvisshal@gmail.com>
2024-02-15 04:39:04 -0500

    first commit

---
+++ polls.App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace polls.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
---
+++ polls.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace polls.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
